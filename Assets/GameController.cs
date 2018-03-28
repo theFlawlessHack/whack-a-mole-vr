642d73de99c4f2de21f7b9d6fdaaec13bb15507a
Jessica Joseph <jessjeanjo@gmail.com>
2018-03-28 18:25:59 -0400

    moles are hiding

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject moleContainer;
+
+    private Mole[] moles;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Mole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+
+    public float visibleHeight = 0.1f;
+    public float hiddenHeight = -0.5f;
+    public float speed = 4f;
+
+    private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            visibleHeight,
+            transform.localPosition.z
+            );
+
+        transform.localPosition = targetPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = Vector3.Lerp( transform.localPosition, targetPosition, Time.deltaTime * speed);
+	}
+
+    public void OnHit () {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z
+        );
+    }
+}
