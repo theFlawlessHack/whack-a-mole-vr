92d223b6971e9172ed96ab3fd81478b70b7318ff
Jessica Joseph <jessjeanjo@gmail.com>
2018-03-28 19:02:52 -0400

    timer and game over are showing

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,34 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
     public GameObject moleContainer;
+    public Player player;
+    public TextMesh InfoText;
+
     public float spawnDuration = 1.5f;
     public float spawnDecrement = 0.1f;
     public float minimumSpawnDuration = 0.5f;
+    public float gameTimer = 15f;
 
     private Mole[] moles;
     private float spawnTimer = 0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         moles = moleContainer.GetComponentsInChildren<Mole>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0f) {
-            moles[Random.Range(0, moles.Length)].Rise();
+    }
 
-            spawnDuration -= spawnDecrement;
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer -= Time.deltaTime;
+        if (gameTimer > 0f)
+        {
 
-            if (spawnDuration < minimumSpawnDuration) {
-                spawnDuration = minimumSpawnDuration;
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0f)
+            {
+                moles[Random.Range(0, moles.Length)].Rise();
+
+                spawnDuration -= spawnDecrement;
+
+                if (spawnDuration < minimumSpawnDuration)
+                {
+                    spawnDuration = minimumSpawnDuration;
+                }
+                spawnTimer = spawnDuration;
             }
-            spawnTimer = spawnDuration;
+
+            InfoText.text = "Hit all the moles!\nTime: " + Mathf.Floor(gameTimer) + "\nScore: " + player.score;
+        }
+        else
+        {
+            InfoText.text = "Game over! Your score: " + Mathf.Floor(player.score);
         }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public int score = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +22,8 @@ public class Player : MonoBehaviour {
                 if (hit.transform.GetComponent<Mole>() != null) {
                     Mole mole = hit.transform.GetComponent<Mole>();
                     mole.OnHit();
+
+                    score++;
                 }
             }
         }
