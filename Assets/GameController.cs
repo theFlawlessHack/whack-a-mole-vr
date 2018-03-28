25264b046113d83cb9e61bb57016310f381377d5
Jessica Joseph <jessjeanjo@gmail.com>
2018-03-28 18:47:56 -0400

    moles are appearing and disappearing correctly

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public GameObject moleContainer;
+    public float spawnDuration = 1.5f;
+    public float spawnDecrement = 0.1f;
+    public float minimumSpawnDuration = 0.5f;
 
     private Mole[] moles;
+    private float spawnTimer = 0f;
 
 	// Use this for initialization
 	void Start () {
         moles = moleContainer.GetComponentsInChildren<Mole>();
 
-        moles[Random.Range(0, moles.Length)].Rise();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f) {
+            moles[Random.Range(0, moles.Length)].Rise();
+
+            spawnDuration -= spawnDecrement;
+
+            if (spawnDuration < minimumSpawnDuration) {
+                spawnDuration = minimumSpawnDuration;
+            }
+            spawnTimer = spawnDuration;
+        }
+    }
 }
